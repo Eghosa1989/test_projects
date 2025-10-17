f403c66702a9c805ead7000275ee8d062ce4d2ae
Eghosa Elaiho <eghosaelaiho@gmail.com>
2025-10-16 18:23:32 -0600

    update

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
 describe('template spec', () => {
-  it('passes', () => {
+  it('Get, types, and assert', () => {
     cy.visit('https://example.cypress.io')
+    cy.contains('type').click()
+    cy.url().should('include', '/commands/actions')
+
+    cy.get('.action-email').type('fakeemail@example.com')
+
+    cy.get('.action-email').should('have.value', 'fakeemail@example.com')
   })
 })
